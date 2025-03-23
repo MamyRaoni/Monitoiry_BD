6f5faa82325572a2ed65592227fe09c00f9e09ba
tingl <mamyraoni@gmail.com>
2025-03-23 10:26:46 +0300

    version stable

--- src/EventSubscriber/ResponseSubscriber.php
+++ src/EventSubscriber/ResponseSubscriber.php
@@ -4,7 +4,6 @@ namespace App\EventSubscriber;
 
 use LoginResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class ResponseSubscriber implements EventSubscriberInterface
 {
